9035ae2d2e61957368521c060b5604981b84e8b1
Damián Pumar <damianpumar@gmail.com>
2024-06-04 19:51:24 +0200

    ✨ Improve

--- apps/starter/src/infrastructure/services/CookiesRepositoryFactoryConcrete.ts
+++ apps/starter/src/infrastructure/services/CookiesRepositoryFactoryConcrete.ts
@@ -10,8 +10,8 @@ export class CookiesRepositoryFactoryConcrete
   extends Factory
   implements CookiesRepositoryFactory
 {
-  create(user: User) {
-    if (user.isAdmin) return this.resolver.resolve(CookiesRepositoryForAdmin);
+  create(user?: User) {
+    if (user?.isAdmin) return this.resolver.resolve(CookiesRepositoryForAdmin);
 
     return this.resolver.resolve(CookiesRepositoryForNormalUser);
   }
